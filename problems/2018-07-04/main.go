a92cc501998a294911f6bf41189d319109a23bca
Nikita Galayko <ngalayko@gmail.com>
2018-07-04 19:11:07 +0200

    2018-07-04: completed

--- problems/2018-07-04/main.go
+++ problems/2018-07-04/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+}
+
+type node struct {
+	Value string `json:"value"`
+	Left  *node  `json:"left"`
+	Right *node  `json:"right"`
+}
+
+// New is a node constructor.
+func New(v string, l, r *node) *node {
+	return &node{
+		Value: v,
+		Left:  l,
+		Right: r,
+	}
+}
+
+// Serialize returns string representation of a node.
+func (n *node) Serialize() (string, error) {
+	b, err := json.Marshal(n)
+	return string(b), err
+}
+
+// Deserialize returns node from it's string representation.
+func Deserialize(s string) (*node, error) {
+	n := new(node)
+	return n, json.Unmarshal([]byte(s), n)
 }
--- problems/2018-07-04/main_test.go
+++ problems/2018-07-04/main_test.go
@@ -1,15 +1,39 @@
 package main
 
-import "testing"
-
-var testCases = []struct {
-	name string
-}{}
+import (
+	"testing"
+)
 
 func Test(t *testing.T) {
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
+	root := New(
+		"root",
+		New(
+			"left",
+			New(
+				"left.left",
+				nil,
+				nil,
+			),
+			nil,
+		),
+		New(
+			"right",
+			nil,
+			nil,
+		),
+	)
 
-		})
+	rootS, err := root.Serialize()
+	if err != nil {
+		t.Fatalf("serialization error: %s", err)
+	}
+
+	node, err := Deserialize(rootS)
+	if err != nil {
+		t.Fatalf("deserialization error: %s", err)
+	}
+
+	if node.Left.Left.Value != "left.left" {
+		t.Fatalf("test failed!")
 	}
 }
