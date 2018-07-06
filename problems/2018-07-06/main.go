9dadb266fad99ec51051b751b5eaaed170d4acf2
Nikita Galayko <ngalayko@gmail.com>
2018-07-06 18:59:55 +0200

    2018-07-06: completed

--- problems/2018-07-06/main.go
+++ problems/2018-07-06/main.go
@@ -3,5 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+
+	car := car(cons(3, 4))
+	cdr := cdr(cons(3, 4))
+
+	fmt.Printf("car: %d, cdr: %d\n", car, cdr)
+}
+
+type choise func(int, int) int
+
+type makeChoise func(choise) int
+
+func cons(a, b int) makeChoise {
+	p := func(i choise) int {
+		return i(a, b)
+	}
+	return p
+}
+
+func car(do makeChoise) int {
+	chooseRight := func(a, b int) int {
+		return a
+	}
+	return do(chooseRight)
+}
+
+func cdr(do makeChoise) int {
+	chooseLeft := func(a, b int) int {
+		return b
+	}
+	return do(chooseLeft)
 }
---
+++ problems/2018-07-06/main.py
@@ -0,0 +1,17 @@
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+def car(f):
+    def left(a, b):
+        return a
+    return f(left)
+
+def cdr(f):
+    def right(a, b):
+        return b
+    return f(right)
+
+print(car(cons(3,4)))
+print(cdr(cons(3,4)))
