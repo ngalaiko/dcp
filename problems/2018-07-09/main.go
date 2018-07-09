8ee0a4476b287bdff49e37b9394f961e396d60e3
Nikita Galayko <ngalayko@gmail.com>
2018-07-09 19:00:08 +0200

    2018-07-09: completed

--- problems/2018-07-09/main.go
+++ problems/2018-07-09/main.go
@@ -5,3 +5,32 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, world!")
 }
+
+type node struct {
+	value       int
+	left, right *node
+}
+
+func unival(n *node) int {
+	if n == nil {
+		return 0
+	}
+
+	uni := true
+
+	switch {
+	case n.left != nil && n.right != nil:
+		uni = n.left.value == n.right.value &&
+			n.right.value == n.value
+	case n.left != nil:
+		uni = n.left.value == n.value
+	case n.right != nil:
+		uni = n.right.value == n.value
+	}
+
+	if !uni {
+		return unival(n.left) + unival(n.right)
+	}
+
+	return unival(n.left) + unival(n.right) + 1
+}
--- problems/2018-07-09/main_test.go
+++ problems/2018-07-09/main_test.go
@@ -3,13 +3,59 @@ package main
 import "testing"
 
 var testCases = []struct {
-	name string
-}{}
+	in  *node
+	out int
+}{
+	{
+		in: &node{
+			value: 0,
+			left:  &node{value: 1},
+			right: &node{
+				value: 0,
+				left: &node{
+					value: 1,
+					left:  &node{value: 1},
+					right: &node{value: 1},
+				},
+				right: &node{value: 0},
+			},
+		},
+		out: 5,
+	},
+	{
+		in: &node{
+			value: 1,
+		},
+		out: 1,
+	},
+	{
+		in: &node{
+			value: 1,
+			left: &node{
+				value: 0,
+			},
+		},
+		out: 1,
+	},
+	{
+		in: &node{
+			value: 1,
+			left: &node{
+				value: 0,
+			},
+			right: &node{
+				value: 1,
+			},
+		},
+		out: 2,
+	},
+}
 
 func Test(t *testing.T) {
 	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-
-		})
+		res := unival(tc.in)
+		if res != tc.out {
+			t.Fatalf("expected %d, got %d", tc.out, res)
+		}
 	}
 }
