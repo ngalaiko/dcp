62bd3f4ac6ed6cc002b2380b578a16ee46c12922
Nikita Galayko <ngalayko@gmail.com>
2018-07-09 09:41:37 +0200

    2018-07-08: completed

--- problems/2018-07-08/main.go
+++ problems/2018-07-08/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+}
+
+func solution(s string) int {
+	l := len(s)
+	switch {
+	case l == 1:
+		return 1
+	case l == 2:
+		return canDecode(s) + 1
+	default:
+		return canDecode(s[:1])*solution(s[1:]) +
+			canDecode(s[:2])*solution(s[2:])
+	}
+}
+
+// returns 1 if possible to decode string.
+func canDecode(s string) int {
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return 0
+	}
+	if i >= 0 && i <= 26 {
+		return 1
+	}
+	return 0
 }
--- problems/2018-07-08/main_test.go
+++ problems/2018-07-08/main_test.go
@@ -3,13 +3,25 @@ package main
 import "testing"
 
 var testCases = []struct {
-	name string
-}{}
+	in  string
+	out int
+}{
+	{"1234", 3},
+	{"111", 3},
+	{"1", 1},
+	{"14", 2},
+	{"143", 2},
+	{"276", 1},
+	{"1212", 5},
+}
 
 func Test(t *testing.T) {
 	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-
+		t.Run(tc.in, func(t *testing.T) {
+			res := solution(tc.in)
+			if res != tc.out {
+				t.Fatalf("expected %d, got %d", tc.out, res)
+			}
 		})
 	}
 }
