3c3003066d0858d9ff2ee51e5b8442e092e87e41
Nikita Galayko <ngalayko@gmail.com>
2018-07-10 19:08:21 +0200

    2018-07-10: completed

--- problems/2018-07-10/main.go
+++ problems/2018-07-10/main.go
@@ -5,3 +5,23 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, world!")
 }
+
+func solution(aa []int) int {
+	maxS := 0
+
+	// 1st or 2nd should always be in sum
+	for start := range make([]byte, 2) {
+		// minimal step is 2
+		for step := 2; step <= len(aa); step += 1 {
+			s := 0
+			for i := start; i < len(aa); i += step {
+				s += aa[i]
+			}
+
+			if s > maxS {
+				maxS = s
+			}
+		}
+	}
+	return maxS
+}
--- problems/2018-07-10/main_test.go
+++ problems/2018-07-10/main_test.go
@@ -1,15 +1,51 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
 
 var testCases = []struct {
 	name string
-}{}
+	in   []int
+	out  int
+}{
+	{
+		name: "example 1",
+		in:   []int{2, 4, 6, 2, 5},
+		out:  13,
+	},
+	{
+		name: "example 2",
+		in:   []int{5, 1, 1, 5},
+		out:  10,
+	},
+}
 
 func Test(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
+			res := solution(tc.in)
+			if res != tc.out {
+				t.Fatalf("expected %d, got %d", tc.out, res)
+			}
+		})
+	}
+}
 
+func Benchmark(b *testing.B) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	for i := 0; i < 10000; i += 1000 {
+		slice := make([]int, i)
+		for j := 0; j < i; j++ {
+			slice[j] = rand.Int()
+		}
+		b.Run(fmt.Sprint(i), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				solution(slice)
+			}
 		})
 	}
 }
