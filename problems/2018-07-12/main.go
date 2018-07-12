ccbacf25664a6cc86c17877f5bb6c7e13a2bc392
Nikita Galayko <ngalayko@gmail.com>
2018-07-12 19:48:14 +0200

    2018-07-12: completed

--- problems/2018-07-12/main.go
+++ problems/2018-07-12/main.go
@@ -5,3 +5,82 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, world!")
 }
+
+func solution(ss []string, p string) []string {
+	tree := newTree()
+	for _, s := range ss {
+		tree.Load(s)
+	}
+
+	res := tree.Find(p)
+	for i, r := range res {
+		res[i] = p + r
+	}
+	return res
+}
+
+type node struct {
+	value rune
+	lefs  map[rune]*node
+}
+
+func newNode(v rune) *node {
+	return &node{
+		value: v,
+		lefs:  map[rune]*node{},
+	}
+}
+
+type tree struct {
+	head *node
+}
+
+func newTree() *tree {
+	return &tree{
+		head: newNode('^'),
+	}
+}
+
+func (t *tree) Find(s string) []string {
+	n := t.head
+	for _, c := range s {
+		n = n.Find(c)
+		if n == nil {
+			return nil
+		}
+	}
+	return n.String()
+}
+
+func (t *tree) Load(s string) {
+	n := t.head
+	for _, c := range s {
+		n = n.Load(c)
+	}
+}
+
+func (n *node) Find(r rune) *node {
+	return n.lefs[r]
+}
+
+func (n *node) Load(r rune) *node {
+	if found, ok := n.lefs[r]; ok {
+		return found
+	}
+	new := newNode(r)
+	n.lefs[r] = new
+	return new
+}
+
+func (n *node) String() []string {
+	r := []string{}
+	for c, l := range n.lefs {
+		if len(l.lefs) == 0 {
+			r = append(r, string(c))
+		}
+		for _, s := range l.String() {
+			r = append(r, string(c)+s)
+		}
+	}
+	return r
+}
--- problems/2018-07-12/main_test.go
+++ problems/2018-07-12/main_test.go
@@ -4,12 +4,53 @@ import "testing"
 
 var testCases = []struct {
 	name string
-}{}
+	in2  string
+	in   []string
+	out  []string
+}{
+	{
+		name: "example 1",
+		in:   []string{"dog", "deer", "deal"},
+		in2:  "de",
+		out:  []string{"deer", "deal"},
+	},
+	{
+		name: "no suggestion",
+		in:   []string{"dog", "deer", "deal"},
+		in2:  "ga",
+		out:  []string{},
+	},
+	{
+		name: "two roots",
+		in:   []string{"dog", "game", "grade"},
+		in2:  "g",
+		out:  []string{"game", "grade"},
+	},
+	{
+		name: "full word",
+		in:   []string{"testing", "test", "a"},
+		in2:  "testing",
+		out:  []string{},
+	},
+}
 
 func Test(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
+			res := solution(tc.in, tc.in2)
+			if len(res) != len(tc.out) {
+				t.Fatalf("expected %v, got %v", tc.out, res)
+			}
 
+			resSet := map[string]bool{}
+			for _, s := range tc.out {
+				resSet[s] = true
+			}
+			for _, r := range res {
+				if !resSet[r] {
+					t.Fatalf("expected %v, got %v", tc.out, res)
+				}
+			}
 		})
 	}
 }
