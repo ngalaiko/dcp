c4bf5d45fb0e15e2a6383dce149565b1e62f5ff0
Nikita Galayko <ngalayko@gmail.com>
2018-07-13 18:31:38 +0200

    2018-07-13: completed

--- problems/2018-07-13/main.go
+++ problems/2018-07-13/main.go
@@ -6,6 +6,19 @@ func main() {
 	fmt.Println("Hello, world!")
 }
 
-func solution(v interface{}) interface{} {
-	return nil
+func solution(n int, xx []int) int {
+	if n == 0 {
+		return 1
+	}
+	var res int
+	for _, x := range xx {
+		switch {
+		case n == x:
+			res += 1
+		case n < x:
+		default:
+			res += solution(n-x, xx)
+		}
+	}
+	return res
 }
--- problems/2018-07-13/main_test.go
+++ problems/2018-07-13/main_test.go
@@ -4,14 +4,46 @@ import "testing"
 
 var testCases = []struct {
 	name string
-	in   interface{}
-	out  interface{}
-}{}
+	inN  int
+	inX  []int
+	out  int
+}{
+	{
+		name: "example",
+		inN:  4,
+		inX:  []int{1, 2},
+		out:  5,
+	},
+	{
+		name: "1 way",
+		inN:  4,
+		inX:  []int{1},
+		out:  1,
+	},
+	{
+		name: "2 way",
+		inN:  4,
+		inX:  []int{1, 4},
+		out:  2,
+	},
+	{
+		name: "0 way",
+		inN:  4,
+		inX:  []int{},
+		out:  0,
+	},
+	{
+		name: "1 more way",
+		inN:  4,
+		inX:  []int{4, 2, 1},
+		out:  6,
+	},
+}
 
 func Test(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			res := solution(tc.in)
+			res := solution(tc.inN, tc.inX)
 			if res != tc.out {
 				t.Fatalf("expected %v, got %v", tc.out, res)
 			}
