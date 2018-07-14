1087d69356d524c7f8fe1bf45941ef4320561a7b
Nikita Galayko <ngalayko@gmail.com>
2018-07-14 18:55:22 +0200

    2018-07-14: completed

--- problems/2018-07-14/main.go
+++ problems/2018-07-14/main.go
@@ -1,11 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
 }
 
-func solution(v interface{}) interface{} {
-	return nil
+func solution(k int, s string) string {
+	var res string
+	for i := range s {
+		for j := i + 1; j <= len(s); j++ {
+			sb := s[i:j]
+			if numberOfDistinctChar(sb) > k {
+				continue
+			}
+			if len(sb) > len(res) {
+				res = sb
+			}
+		}
+	}
+	return res
+}
+
+func numberOfDistinctChar(s string) int {
+	m := make(map[rune]struct{}, len(s))
+	for _, c := range s {
+		m[c] = struct{}{}
+	}
+	return len(m)
 }
--- problems/2018-07-14/main_test.go
+++ problems/2018-07-14/main_test.go
@@ -4,14 +4,52 @@ import "testing"
 
 var testCases = []struct {
 	name string
-	in   interface{}
-	out  interface{}
-}{}
+	inS  string
+	inK  int
+	out  string
+}{
+	{
+		name: "example",
+		inS:  "abcba",
+		inK:  2,
+		out:  "bcb",
+	},
+	{
+		name: "3",
+		inS:  "abcba",
+		inK:  3,
+		out:  "abcba",
+	},
+	{
+		name: "4",
+		inS:  "abcba",
+		inK:  4,
+		out:  "abcba",
+	},
+	{
+		name: "5",
+		inS:  "abcba",
+		inK:  5,
+		out:  "abcba",
+	},
+	{
+		name: "0",
+		inS:  "abcba",
+		inK:  0,
+		out:  "",
+	},
+	{
+		name: "1",
+		inS:  "abcba",
+		inK:  1,
+		out:  "a",
+	},
+}
 
 func Test(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			res := solution(tc.in)
+			res := solution(tc.inK, tc.inS)
 			if res != tc.out {
 				t.Fatalf("expected %v, got %v", tc.out, res)
 			}
