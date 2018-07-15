3ea7e684f279d15636e507593c4e6ba4f4e1a761
Nikita Galayko <ngalayko@gmail.com>
2018-07-15 21:06:13 +0200

    2018-07-15: completed

--- problems/2018-07-15/main.go
+++ problems/2018-07-15/main.go
@@ -1,11 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+// n - number of points inside square
+// m - number of points inside circle
+// pi = 4 * m / n
 func main() {
-	fmt.Println("Hello, world!")
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	start := time.Now()
+	fmt.Println(Pi(10000000), time.Since(start))
+	// 3.1420611999999988 1.306183355s
 }
 
-func solution(v interface{}) interface{} {
-	return nil
+func Pi(n int) float64 {
+	ng := 1000
+	if n <= ng {
+		return pi(float64(ng))
+	}
+	n = n / ng
+
+	pis := make(chan float64, ng)
+	for i := 0; i < ng; i++ {
+		go func() {
+			pis <- pi(float64(n))
+		}()
+	}
+
+	var s float64
+	var i int
+	for p := range pis {
+		s += p
+		i++
+		if i == ng {
+			return s / float64(ng)
+		}
+	}
+	return 0
+}
+
+func pi(n float64) float64 {
+	var inCircle float64
+	for i := 1.0; i < n; i++ {
+		x, y := rand.Float64(), rand.Float64()
+
+		insideCircle := (x-1)*(x-1)+(y-1)*(y-1) < 1
+		if insideCircle {
+			inCircle++
+		}
+	}
+	return 4.0 * inCircle / n
 }
