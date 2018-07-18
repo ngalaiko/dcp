3b6186b7a273ccca80890c1736ea30f9af39b667
Nikita Galayko <ngalayko@gmail.com>
2018-07-18 20:34:28 +0200

    2018-07-18: completed

--- problems/2018-07-18/main.go
+++ problems/2018-07-18/main.go
@@ -1,11 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"path"
+	"regexp"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
 }
 
-func solution(v interface{}) interface{} {
-	return nil
+var re = regexp.MustCompile("\t")
+
+func solution(p string) int {
+	splitN := strings.Split(p, "\n")
+	var max int
+	for i := 0; i < len(splitN); i++ {
+		if path.Ext(splitN[i]) == "" {
+			continue
+		}
+
+		pathToFile := make([]string, i+1)
+		for j := 0; j <= i; j++ {
+			level := len(re.FindAllString(splitN[j], -1))
+			pathToFile[level] = splitN[j]
+		}
+		var length int
+		for _, s := range pathToFile {
+			s = strings.Replace(s, "\t", "", -1)
+			if len(s) == 0 {
+				continue
+			}
+			length += len(s) + 1
+		}
+		if length-1 > max {
+			max = length - 1
+		}
+	}
+	return max
 }
--- problems/2018-07-18/main_test.go
+++ problems/2018-07-18/main_test.go
@@ -4,9 +4,30 @@ import "testing"
 
 var testCases = []struct {
 	name string
-	in   interface{}
-	out  interface{}
-}{}
+	in   string
+	out  int
+}{
+	{
+		name: "example",
+		in:   "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext",
+		out:  20,
+	},
+	{
+		name: "example 2",
+		in:   "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext",
+		out:  32,
+	},
+	{
+		name: "file in first dir",
+		in:   "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\t\t\tfile2.ext\n\tsubdir2\n\t\tsubsubdir2",
+		out:  32,
+	},
+	{
+		name: "no file",
+		in:   "dir\n\tsubdir1\n\tsubdir2",
+		out:  0,
+	},
+}
 
 func Test(t *testing.T) {
 	for _, tc := range testCases {
