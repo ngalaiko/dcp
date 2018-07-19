5de229a30fd176266ce13b67a446ab1c909c6bb3
Nikita Galayko <ngalayko@gmail.com>
2018-07-19 21:54:51 +0200

    2018-07-19: completed

--- problems/2018-07-19/main.go
+++ problems/2018-07-19/main.go
@@ -6,6 +6,17 @@ func main() {
 	fmt.Println("Hello, world!")
 }
 
-func solution(v interface{}) interface{} {
-	return nil
+func solution(aa []int, k int) []int {
+	res := make([]int, 0, len(aa)-k-1)
+	for i := 0; i < len(aa)-k; i++ {
+		m := 0
+		for j := i; j <= i+k; j++ {
+			if aa[j] > m {
+				m = aa[j]
+			}
+		}
+		res = append(res, m)
+		fmt.Println(aa[i:i+k], m)
+	}
+	return res
 }
--- problems/2018-07-19/main_test.go
+++ problems/2018-07-19/main_test.go
@@ -1,18 +1,29 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 var testCases = []struct {
 	name string
-	in   interface{}
-	out  interface{}
-}{}
+	inN  int
+	inX  []int
+	out  []int
+}{
+	{
+		name: "example",
+		inN:  2,
+		inX:  []int{10, 5, 2, 7, 8, 7},
+		out:  []int{10, 7, 8, 8},
+	},
+}
 
 func Test(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			res := solution(tc.in)
-			if res != tc.out {
+			res := solution(tc.inX, tc.inN)
+			if fmt.Sprint(res) != fmt.Sprint(tc.out) {
 				t.Fatalf("expected %v, got %v", tc.out, res)
 			}
 		})
