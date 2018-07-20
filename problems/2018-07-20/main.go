0998fbd99a90e05e8b888435f23a46e634f94bed
Nikita Galayko <ngalayko@gmail.com>
2018-07-20 19:19:26 +0200

    2018-07-20: completed

--- problems/2018-07-20/main.go
+++ problems/2018-07-20/main.go
@@ -6,6 +6,16 @@ func main() {
 	fmt.Println("Hello, world!")
 }
 
-func solution(v interface{}) interface{} {
-	return nil
+func solution(mm [][]int) int {
+	n, m := len(mm), len(mm[0])
+	if m > n {
+		n, m = m, n
+	}
+
+	rowSum1 := n / 2 * 3 // sum of odd row
+	if n%2 == 0 {
+		return rowSum1 * m
+	}
+
+	return m/2*(rowSum1+1) + m/2*(rowSum1+2) + m%2*(rowSum1+1)
 }
--- problems/2018-07-20/main_test.go
+++ problems/2018-07-20/main_test.go
@@ -4,9 +4,124 @@ import "testing"
 
 var testCases = []struct {
 	name string
-	in   interface{}
-	out  interface{}
-}{}
+	in   [][]int
+	out  int
+}{
+	{
+		name: "1x1",
+		in: [][]int{
+			[]int{1},
+		},
+		out: 1,
+	},
+	{
+		name: "1x2",
+		in: [][]int{
+			[]int{1, 2},
+		},
+		out: 3,
+	},
+	{
+		name: "1x3",
+		in: [][]int{
+			[]int{1, 2, 1},
+		},
+		out: 4,
+	},
+	{
+		name: "1x4",
+		in: [][]int{
+			[]int{1, 2, 1, 2},
+		},
+		out: 6,
+	},
+	{
+		name: "1x5",
+		in: [][]int{
+			[]int{1, 2, 1, 2, 1},
+		},
+		out: 7,
+	},
+	{
+		name: "1x6",
+		in: [][]int{
+			[]int{1, 2, 1, 2, 1, 2},
+		},
+		out: 9,
+	},
+	{
+		name: "1x7",
+		in: [][]int{
+			[]int{1, 2, 1, 2, 1, 2, 1},
+		},
+		out: 10,
+	},
+	{
+		name: "2x2",
+		in: [][]int{
+			[]int{1, 2},
+			[]int{2, 1},
+		},
+		out: 6,
+	},
+	{
+		name: "2x3",
+		in: [][]int{
+			[]int{1, 2, 1},
+			[]int{2, 1, 2},
+		},
+		out: 9,
+	},
+	{
+		name: "3x3",
+		in: [][]int{
+			[]int{1, 2, 1},
+			[]int{2, 1, 2},
+			[]int{1, 2, 1},
+		},
+		out: 13,
+	},
+	{
+		name: "2x4",
+		in: [][]int{
+			[]int{1, 2, 1, 2},
+			[]int{2, 1, 2, 1},
+		},
+		out: 12,
+	},
+	{
+		name: "3x4",
+		in: [][]int{
+			[]int{1, 2, 1, 2},
+			[]int{2, 1, 2, 1},
+			[]int{1, 2, 1, 2},
+		},
+		out: 18,
+	},
+	{
+		name: "4x4",
+		in: [][]int{
+			[]int{1, 2, 1, 2},
+			[]int{2, 1, 2, 1},
+			[]int{1, 2, 1, 2},
+			[]int{2, 1, 2, 1},
+		},
+		out: 24,
+	},
+	{
+		name: "7x6",
+		in: [][]int{
+			[]int{1, 2, 1, 2, 1, 2},
+			[]int{2, 1, 2, 1, 2, 1},
+			[]int{1, 2, 1, 2, 1, 2},
+			[]int{2, 1, 2, 1, 2, 1},
+			[]int{1, 2, 1, 2, 1, 2},
+			[]int{2, 1, 2, 1, 2, 1},
+			[]int{1, 2, 1, 2, 1, 2},
+		},
+		out: 63,
+	},
+}
 
 func Test(t *testing.T) {
 	for _, tc := range testCases {
