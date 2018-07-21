935ae4b663b7393a09678ae700cf456d75b44377
Nikita Galayko <ngalayko@gmail.com>
2018-07-21 19:17:24 +0200

    2018-07-21: completed

--- problems/2018-07-21/main.go
+++ problems/2018-07-21/main.go
@@ -6,6 +6,38 @@ func main() {
 	fmt.Println("Hello, world!")
 }
 
-func solution(v interface{}) interface{} {
-	return nil
+func solution(a, b *node) int {
+	a = flip(a)
+	b = flip(b)
+	var res int
+	for a != nil && b != nil && a.value == b.value {
+		res = a.value
+		a = a.next
+		b = b.next
+	}
+	return res
+}
+
+func flip(n *node) *node {
+	var c *node = nil
+	for n != nil {
+		next := n.next
+		n.next = c
+
+		c = n
+		n = next
+	}
+	return c
+}
+
+type node struct {
+	value int
+	next  *node
+}
+
+func newNode(v int, next *node) *node {
+	return &node{
+		value: v,
+		next:  next,
+	}
 }
--- problems/2018-07-21/main_test.go
+++ problems/2018-07-21/main_test.go
@@ -4,14 +4,34 @@ import "testing"
 
 var testCases = []struct {
 	name string
-	in   interface{}
-	out  interface{}
-}{}
+	in1  *node
+	in2  *node
+	out  int
+}{
+	{
+		name: "example",
+		in1:  newNode(3, newNode(7, newNode(8, newNode(10, nil)))),
+		in2:  newNode(99, newNode(1, newNode(8, newNode(10, nil)))),
+		out:  8,
+	},
+	{
+		name: "same",
+		in1:  newNode(1, nil),
+		in2:  newNode(1, nil),
+		out:  1,
+	},
+	{
+		name: "duplicate",
+		in1:  newNode(8, newNode(10, newNode(8, newNode(10, nil)))),
+		in2:  newNode(99, newNode(1, newNode(8, newNode(10, nil)))),
+		out:  8,
+	},
+}
 
 func Test(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			res := solution(tc.in)
+			res := solution(tc.in1, tc.in2)
 			if res != tc.out {
 				t.Fatalf("expected %v, got %v", tc.out, res)
 			}
