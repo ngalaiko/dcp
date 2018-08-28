232661b2759a8ee944237f1d520cd3b3abd3a3bb
Nikita Galayko <ngalayko@gmail.com>
2018-08-28 19:28:26 +0200

    2018-08-28: started

--- problems/2018-08-28/main.go
+++ problems/2018-08-28/main.go
@@ -6,6 +6,6 @@ func main() {
 	fmt.Println("Hello, world!")
 }
 
-func solution(maze []int[]int, start, finish []int) int {
+func solution(maze []int[]int, start[]int, finish []int) int {
 	return nil
 }
