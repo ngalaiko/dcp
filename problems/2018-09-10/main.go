4462a5ce2c60e9821c4f133445208df26a9cbaa3
Nikita Galayko <ngalayko@gmail.com>
2018-09-10 16:10:00 +0200

    2018-09-10: completed

--- problems/2018-09-10/main.go
+++ problems/2018-09-10/main.go
@@ -7,13 +7,34 @@ func main() {
 }
 
 func solution(reg string, s string) bool {
-	for _, rc := range reg {
-		switch rc {
+	currentIndex := 0
+	var i int
+	for i = 0; i < len(reg); i++ {
+		switch reg[i] {
 		case '.':
+			// skip 1 char
+			currentIndex++
 		case '*':
+			if i == len(reg)-1 {
+				// in case * is the last symbol in regex, it is valid anyway
+				return true
+			}
+			// skip chars until we have same number of
+			// chars in regex after *
+			// and string after current index
+			until := len(s) - (len(reg) - i - 1)
+			for currentIndex < until && s[currentIndex] != reg[i+1] {
+				currentIndex++
+			}
 		default:
+			// check if valid char at position
+			if reg[i] != s[currentIndex] {
+				return false
+			}
+			currentIndex++
 		}
 	}
 
-	return false
+	// if string was completely validated unsing complete regex, return true
+	return currentIndex == len(s) && i == len(reg)
 }
--- problems/2018-09-10/main_test.go
+++ problems/2018-09-10/main_test.go
@@ -16,7 +16,56 @@ func Test_solution(t *testing.T) {
 
 		want1 bool
 	}{
-		//TODO: Add test cases
+		{
+			name:  ". test 1",
+			args:  func(t *testing.T) args { return args{"ra.", "ray"} },
+			want1: true,
+		},
+		{
+			name:  ". test 2",
+			args:  func(t *testing.T) args { return args{"ra.a", "raya"} },
+			want1: true,
+		},
+		{
+			name:  ". test 3",
+			args:  func(t *testing.T) args { return args{".a.a", "raya"} },
+			want1: true,
+		},
+		{
+			name:  ". test 4",
+			args:  func(t *testing.T) args { return args{"ra.", "raymond"} },
+			want1: false,
+		},
+		{
+			name:  ".* test 1",
+			args:  func(t *testing.T) args { return args{".*at", "chat"} },
+			want1: true,
+		},
+		{
+			name:  ".* test 2",
+			args:  func(t *testing.T) args { return args{".*", "chat"} },
+			want1: true,
+		},
+		{
+			name:  "*. test 3",
+			args:  func(t *testing.T) args { return args{"*.", "chat"} },
+			want1: true,
+		},
+		{
+			name:  ".* test 4",
+			args:  func(t *testing.T) args { return args{".*at", "chats"} },
+			want1: false,
+		},
+		{
+			name:  "* test 1",
+			args:  func(t *testing.T) args { return args{"*", "chats"} },
+			want1: true,
+		},
+		{
+			name:  "* test 2",
+			args:  func(t *testing.T) args { return args{"*", ""} },
+			want1: true,
+		},
 	}
 
 	for _, tt := range tests {
