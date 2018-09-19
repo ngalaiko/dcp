df626091f1a1776ef990b633574c0eb688f27f1e
Nikita Galayko <ngalayko@gmail.com>
2018-09-19 11:39:44 +0400

    2018-09-19: completed

--- problems/2018-09-19/main.go
+++ problems/2018-09-19/main.go
@@ -6,7 +6,79 @@ func main() {
 	fmt.Println("Hello, world!")
 }
 
-func solution(s string) bool {
+func solution(ss string) bool {
+	stack := &Stack{}
+	for _, r := range ss {
+		switch {
+		case open[r]:
+			stack.Push(r)
+		case close[r]:
+			top := stack.Pop()
 
-	return true
+			if top != reverse(r) {
+				return false
+			}
+		}
+	}
+	return stack.Len() == 0
+}
+
+//
+// helpers
+//
+
+// sets with open and closing brackets
+var (
+	open = map[rune]bool{
+		'(': true,
+		'{': true,
+		'[': true,
+	}
+	close = map[rune]bool{
+		')': true,
+		'}': true,
+		']': true,
+	}
+)
+
+func reverse(r rune) rune {
+	switch r {
+	case '(':
+		return ')'
+	case ')':
+		return '('
+	case '[':
+		return ']'
+	case ']':
+		return '['
+	case '{':
+		return '}'
+	case '}':
+		return '{'
+	default:
+		return ' '
+	}
+}
+
+//
+// Classic stack implementation on top of a slice.
+//
+
+type Stack struct {
+	a []rune
+}
+
+func (s *Stack) Push(r rune) {
+	s.a = append(s.a, r)
+}
+
+func (s *Stack) Pop() rune {
+	end := len(s.a) - 1
+	r := s.a[end]
+	s.a = s.a[:end]
+	return r
+}
+
+func (s *Stack) Len() int {
+	return len(s.a)
 }
--- problems/2018-09-19/main_test.go
+++ problems/2018-09-19/main_test.go
@@ -15,7 +15,34 @@ func Test_solution(t *testing.T) {
 
 		want1 bool
 	}{
-		//TODO: Add test cases
+		{
+			name: "example 1",
+			args: func(t *testing.T) args {
+				return args{s: "([])[]({})"}
+			},
+			want1: true,
+		},
+		{
+			name: "example 2",
+			args: func(t *testing.T) args {
+				return args{s: "([)]"}
+			},
+			want1: false,
+		},
+		{
+			name: "example 3",
+			args: func(t *testing.T) args {
+				return args{s: "((()"}
+			},
+			want1: false,
+		},
+		{
+			name: "empty strung",
+			args: func(t *testing.T) args {
+				return args{s: ""}
+			},
+			want1: true,
+		},
 	}
 
 	for _, tt := range tests {
