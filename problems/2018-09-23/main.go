9832839e5d722f57ce53a166c77aa21369833af2
Nikita Galayko <ngalayko@gmail.com>
2018-09-23 12:57:44 +0200

    2018-09-23: started

--- problems/2018-09-23/main.go
+++ problems/2018-09-23/main.go
@@ -1,11 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
 }
 
-func solution(v interface{}) interface{} {
-	return nil
+func solution(in string) string {
+	out := strings.Builder{}
+	currentChar := in[0]
+	count := 0
+	for _, c := range in {
+		switch c {
+		case currentChar:
+			count++
+		default:
+			out.WriteString(fmt.Sprintf("%d%c"), count, currentChar)
+			currentChar = c
+			count = 1
+		}
+	}
+	return out.String()
 }
